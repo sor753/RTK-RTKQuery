7e4a0d33360e14c8e8bdd0b25c8afaccb11a9ee3
show kawazoe <show753@icloud.com>
2025-09-23 13:03:41 +0900

    Structuring Project

--- rtk-crud/src/App.tsx
+++ rtk-crud/src/App.tsx
@@ -1,9 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import "./App.css"
+import Home from "./pages/Home"
+import AddEdit from "./pages/AddEdit"
+import Info from "./pages/Info"
 
 const App = () => {
   return (
     <div className="App">
-      <h2>RTK Query</h2>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/addContact" element={<AddEdit />} />
+          <Route path="/editContact/:id" element={<AddEdit />} />
+          <Route path="/info/:id" element={<Info />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ rtk-crud/src/pages/AddEdit.tsx
@@ -0,0 +1,9 @@
+const AddEdit = () => {
+  return (
+    <div>
+      <h2>Add/Edit Contact</h2>
+    </div>
+  )
+}
+
+export default AddEdit
