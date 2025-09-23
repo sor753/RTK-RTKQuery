4986ad90f3c964cc7ea9cd673b007a95fdbc99e1
show kawazoe <show753@icloud.com>
2025-09-24 06:12:34 +0900

    Navbar & Search Input Field

--- rtk-recipe/src/App.tsx
+++ rtk-recipe/src/App.tsx
@@ -1,7 +1,40 @@
+import { useState } from "react"
 import "./App.css"
 
-export const App = () => (
-  <div className="App">
-    <h2>Food Recipe</h2>
-  </div>
-)
+export const App = () => {
+  const [value, setValue] = useState("")
+  const [query, setQuery] = useState("")
+  const [show, setShow] = useState(false)
+  const [recipes, setRecipes] = useState({})
+
+  return (
+    <div
+      className="App"
+      style={{
+        margin: "auto",
+        padding: "15px",
+        maxWidth: "1000px",
+        alignContent: "center",
+      }}
+    >
+      <nav>
+        <h2>Food Recipe</h2>
+      </nav>
+      <div className="row g-1 align-items-center mt-2">
+        <label htmlFor="recipe-search">Search Recipes</label>
+        <input
+          type="text"
+          id="recipe-search"
+          name="recipe-search"
+          value={value}
+          onChange={e => {
+            setValue(e.target.value)
+          }}
+        />
+        <div className="col-auto">
+          <button>Search</button>
+        </div>
+      </div>
+    </div>
+  )
+}
