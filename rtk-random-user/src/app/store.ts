f84f9bdede69d40daae41553cd2f1b9b2d86f732
show kawazoe <show753@icloud.com>
2025-09-23 11:03:37 +0900

    Configure RTK and Writing 1st Query

--- rtk-random-user/src/app/store.ts
+++ rtk-random-user/src/app/store.ts
@@ -1,3 +1,18 @@
-import type { Action, ThunkAction } from "@reduxjs/toolkit"
-import { combineSlices, configureStore } from "@reduxjs/toolkit"
+import { combineReducers, configureStore } from "@reduxjs/toolkit"
+import { usersApi } from "../services/users"
 import { setupListeners } from "@reduxjs/toolkit/query"
+
+const rootReducer = combineReducers({
+  [usersApi.reducerPath]: usersApi.reducer,
+})
+
+export const store = configureStore({
+  reducer: rootReducer,
+  // アクションのディスパッチからリデューサーに到達するまでの間に、サードパーティ製の拡張ポイントを提供します。
+  // Reduxミドルウェアは、ログ記録、クラッシュレポート、非同期APIとの通信、ルーティングなどに利用されています。
+  middleware: getDefaultMiddleware =>
+    // getDefaultMiddleware：カスタムミドルウェアを追加したいが、デフォルトのミドルウェアも追加しておきたい場合
+    getDefaultMiddleware().concat(usersApi.middleware),
+})
+
+setupListeners(store.dispatch)
--- rtk-random-user/src/main.tsx
+++ rtk-random-user/src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-// import { Provider } from "react-redux"
+import { Provider } from "react-redux"
 import { App } from "./App"
-// import { store } from "./app/store"
+import { store } from "./app/store"
 import "./index.css"
 
 const container = document.getElementById("root")
@@ -12,9 +12,9 @@ if (container) {
 
   root.render(
     <StrictMode>
-      {/* <Provider store={store}> */}
-      <App />
-      {/* </Provider> */}
+      <Provider store={store}>
+        <App />
+      </Provider>
     </StrictMode>,
   )
 } else {
